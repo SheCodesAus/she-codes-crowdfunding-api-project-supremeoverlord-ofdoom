80d90ae2c6356fd70fda9cee31b80ba54925a277
supremeoverlord-ofdoom <108652766+supremeoverlord-ofdoom@users.noreply.github.com>
2023-01-15 14:32:19 +0800

    linked pledges to projects
    from Handling Model Relationships step 4 onwards complete

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 
 from .models import Project, Pledge
 
+class PledgeSerializer(serializers.ModelSerializer): #doing the serializer no manually
+    class Meta:
+        model = Pledge
+        fields = ['id', 'amount', 'comment', 'anonymous', 'project', 'supporter']
+
 class ProjectSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     title = serializers.CharField(max_length=200)
@@ -11,13 +16,12 @@ class ProjectSerializer(serializers.Serializer):
     is_open = serializers.BooleanField()
     date_created = serializers.DateTimeField()
     owner = serializers.CharField(max_length=200)
+    #pledges = PledgeSerializer(many=True, read_only=True) #so we can see the pledges for each project
 
     def create(self, validated_data):
         return Project.objects.create(**validated_data) #returns key values pairs - title = string, descrption = value ect
 
-class PledgeSerializer(serializers.ModelSerializer): #doing the serializer no manually
-    class Meta:
-        model = Pledge
-        fields = ['id', 'amount', 'comment', 'anonymous', 'project', 'supporter']
-        
+
+class ProjectDetailSerializer(ProjectSerializer): #pledges linked to each project split off 
+    pledges = PledgeSerializer(many=True, read_only=True)  #reducing the amount of data we are fetching when viewing allprojects     
 
