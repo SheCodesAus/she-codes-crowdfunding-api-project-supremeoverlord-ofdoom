aceb4a60d719a76aeacbb0b66c7bee1d6382f173
supremeoverlord-ofdoom <108652766+supremeoverlord-ofdoom@users.noreply.github.com>
2023-01-20 13:37:13 +0800

    adding permissions
    all steps from permissions document applied, working so far

---
+++ crowdfunding/projects/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+#making a custom permission so that only the owner of a project can edit it:
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS: #is the logged in user the owner
+            return True
+        return obj.owner == request.user #if that's true - then can edit
--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
-from rest_framework import status, generics
+from rest_framework import status, generics, permissions
 
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 class ProjectList(APIView): #project is going to be in long form (this is what is going on under the hood)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request): #using get here #when you get a get requets this is what you do
         projects = Project.objects.all()
@@ -23,10 +25,16 @@ class ProjectList(APIView): #project is going to be in long form (this is what i
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ProjectDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly #adding to view - only the owner of a project can edit it
+        ]
 
     def get_object(self, pk):
         try:
-            return Project.objects.get(pk=pk) #pk = the variable we give it is the primary ley
+            project = Project.objects.get(pk=pk) #pk = the variable we give it is the primary ley
+            self.check_object_permissions(self.request, project) #extra layer of checking permissions
+            return project 
         except Project.DoesNotExist:
             raise Http404
 
@@ -34,6 +42,19 @@ class ProjectDetail(APIView):
         project = self.get_object(pk)
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
+    
+    def put(self, request, pk): #ability to edit
+        project = self.get_object(pk)
+        data = request.data
+        serializer = ProjectDetailSerializer(
+        instance=project,
+        data=data,
+        partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
 
 class PledgeList(generics.ListCreateAPIView): #pledge is going to be shown in short efficient form (here is your car who carts whats in it)
     queryset = Pledge.objects.all()
