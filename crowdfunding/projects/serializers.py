8a6eceb11852ba6af3e8ca12c6e61e2af7d1c737
supremeoverlord-ofdoom <108652766+supremeoverlord-ofdoom@users.noreply.github.com>
2023-01-24 19:05:28 +0800

    adding conditional logic for anoymous pledges
    this now works- if anonymous pledge, then rename supporter field

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -3,12 +3,89 @@ from rest_framework import serializers
 from .models import Project, Pledge
 
 class PledgeSerializer(serializers.ModelSerializer): #doing the serializer no manually
+    supporter = serializers.SerializerMethodField()
     class Meta:
         model = Pledge
         fields = ['id', 'amount', 'comment', 'anonymous', 'project', 'supporter'] 
-        read_only_fields = ['id', 'supporter']
-        if 'anonymous' == 1:
-            exclude = ['supporter']
+        read_only_fields = ['id']
+        
+    def get_supporter(self, obj):
+        if obj.anonymous:
+            return f"mystery gnome enthusiast"
+        else:
+            return obj.supporter.username
+        
+    def create(self, validated_data):
+        return Pledge.objects.create(**validated_data)
+
+# class PledgeSerializer(serializers.ModelSerializer): #doing the serializer no manually
+#     class Meta:
+#         model = Pledge
+#         fields = ['id', 'amount', 'comment', 'anonymous', 'project', 'supporter'] 
+#         read_only_fields = ['id']
+#         if 'anonymous' == 'true':
+#             extra_kwargs = {'supporter': {'write_only': True}}
+#         else:
+#             read_only_fields = ['id','supporter']
+        
+#     def create(self, validated_data):
+#         return Pledge.objects.create(**validated_data)
+
+#this removes all supporters 
+# class PledgeSerializer(serializers.ModelSerializer): #doing the serializer no manually
+#     class Meta:
+#         model = Pledge
+#         fields = ['id', 'amount', 'comment', 'anonymous', 'project', 'supporter'] 
+#         read_only_fields = ['id']
+#         extra_kwargs = {'supporter': {'write_only': True}}
+
+#     def create(self, validated_data):
+#         return Pledge.objects.create(**validated_data)   
+    
+
+#none of this works
+
+# class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+#     """
+#     A ModelSerializer that takes an additional `fields` argument that
+#     controls which fields should be displayed.
+#     """
+
+#     def __init__(self, *args, **kwargs):
+#         # Don't pass the 'fields' arg up to the superclass
+#         fields = kwargs.pop('fields', None)
+
+#         # Instantiate the superclass normally
+#         super().__init__(*args, **kwargs)
+
+#         if fields is not None:
+#             # Drop any fields that are not specified in the `fields` argument.
+#             allowed = set(fields)
+#             existing = set(self.fields)
+#             for field_name in existing - allowed:
+#                 self.fields.pop(field_name)
+
+# class PledgeSerializer(serializers.ModelSerializer): #doing the serializer no manually
+#     class Meta:
+#         model = Pledge
+#         if 'anonymous' == 1:
+#             fields = ['id', 'amount', 'comment', 'anonymous', 'project'] 
+#             read_only_fields = ['id']
+#         else:
+#             fields = ['id', 'amount', 'comment', 'anonymous', 'project', 'supporter'] 
+#             read_only_fields = ['id', 'supporter']
+        
+#     def create(self, validated_data):
+#         return Pledge.objects.create(**validated_data)
+
+
+# class PledgeSerializer(serializers.ModelSerializer): #doing the serializer no manually
+#     class Meta:
+#         model = Pledge
+#         fields = ['id', 'amount', 'comment', 'anonymous', 'project', 'supporter'] 
+#         read_only_fields = ['id', 'supporter']
+#         if 'anonymous' == 1:
+#             exclude = ['supporter']
 
             
 
